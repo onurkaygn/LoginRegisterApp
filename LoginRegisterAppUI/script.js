505c30954259227310221b337e79abf6088bae0a
Onuralp Kaygın <onuralpkayginedu@gmail.com>
2023-12-18 23:14:58 +0300

    backend connected

--- LoginRegisterAppUI/script.js
+++ LoginRegisterAppUI/script.js
@@ -1,17 +1,50 @@
-function register() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+async function register() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const user = {
+      username: username,
+      password: password
+    } 
 
     // Kullanıcı kayıt işlemleri burada gerçekleştirilebilir
 
-    alert('Kayıt başarılı! E-mail: ' + email);
+    const response = await fetch("https://localhost:7200/api/Auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+    });
+    if (response.status === 200) {
+      alert('Kayıt başarılı! Username: ' + username);
+    }else {
+      alert('Kayıt başarısız!');
+    }
   }
 
-  function login() {
-    var loginEmail = document.getElementById('loginEmail').value;
-    var loginPassword = document.getElementById('loginPassword').value;
+async function login() {
+  const username = document.getElementById('loginUsername').value;
+  const password = document.getElementById('loginPassword').value;
 
-    // Kullanıcı giriş işlemleri burada gerçekleştirilebilir
-
-    alert('Giriş başarılı! E-mail: ' + loginEmail);
+  const model = {
+    username: username,
+    password: password
   }
+
+  const response = await fetch("https://localhost:7200/api/Auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(model)
+  });
+
+  if (response.status === 200) {
+    alert('Giriş başarılı! Username: ' + username);
+  }else if (response.status === 400) {
+    alert('Parola yanlış.');
+  }else {
+    alert('Giriş başarısız!');
+  }
+}
