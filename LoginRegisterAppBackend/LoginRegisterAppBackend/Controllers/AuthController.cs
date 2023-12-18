092979a8e1370914ac48269c663d19b0fda14559
Onuralp Kaygın <onuralpkayginedu@gmail.com>
2023-12-18 22:39:47 +0300

    database connected and dapper added

--- LoginRegisterAppBackend/LoginRegisterAppBackend/Controllers/AuthController.cs
+++ LoginRegisterAppBackend/LoginRegisterAppBackend/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LoginRegisterAppBackend.Models;
 using Microsoft.AspNetCore.Mvc;
-using LoginRegisterAppBackend.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace LoginRegisterAppBackend.Controllers
 {
@@ -8,30 +10,54 @@ namespace LoginRegisterAppBackend.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpPost("register")]
-        public  ActionResult<User> Register(UserDto request)
+        public ActionResult<User> Register(UserDto request)
         {
             string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
 
-            user.Username = request.Username;
-            user.PasswordHash = passwordHash;
-
-            return Ok(user);
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DbConnection")))
+            {
+                connection.Open();
+                connection.ExecuteAsync("SP_CreateUser", 
+                    new {
+                        Username = request.Username,
+                        PasswordHash = passwordHash
+                    },
+                    commandType: CommandType.StoredProcedure);
+                return Ok(true);
+            }
         }
 
         [HttpPost("login")]
-        public ActionResult<User> Login(UserDto request)
+        public async Task<ActionResult<User>> Login(UserDto request)
         {
-            if(user.Username != request.Username) {
-                return BadRequest("User not found.");
-            }
-            if(!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DbConnection")))
             {
-                return BadRequest("Wrong password.");
+                connection.Open();
+                var user = await connection.QueryFirstOrDefaultAsync<User>("SP_GetUserByUsername",
+                    new { Username = request.Username },
+                    commandType: CommandType.StoredProcedure);
+
+                if (user == null)
+                {
+                    return NotFound("User with the specified name was not found on the server.");
+                }
+
+                if (BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+                {
+                    return Ok();
+                }else
+                {
+                    return BadRequest("Wrong password.");
+                }
             }
-            return Ok(user);
         }
 
     }
